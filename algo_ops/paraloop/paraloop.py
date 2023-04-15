23b7e88b58e21109d8d8e43136fd7e52629ec439
Prateek Tandon <prateek1.tandon@gmail.com>
2023-04-15 16:53:54 -0700

    fix loop

--- algo_ops/paraloop/paraloop.py
+++ algo_ops/paraloop/paraloop.py
@@ -1,5 +1,5 @@
 import multiprocessing
-from typing import Callable, List, Any, Sequence
+from typing import Callable, List, Any, Sequence, Optional
 
 
 def _sequential(
@@ -24,32 +24,44 @@ def _sequential(
     return results
 
 
-def _pool(func: Callable, params: Sequence[Any]) -> List[Any]:
+def _pool(
+    func: Callable, params: Sequence[Any], num_cores: Optional[int] = None
+) -> List[Any]:
     """
     Implements paraloop using multiprocessing pool.
 
     param func: The function to parallelize
     param params: Inputs to function
+    param num_cores: Number of cores to use
+
     return:
         Results of function executions
     """
-    num_cores = multiprocessing.cpu_count()
+    if num_cores is None:
+        num_cores = multiprocessing.cpu_count()
     with multiprocessing.Pool(num_cores) as p:
         return p.map(func, params)
 
 
-def loop(func: Callable, params: Sequence[Any], mechanism: str = "pool") -> List[Any]:
+def loop(
+    func: Callable,
+    params: Sequence[Any],
+    mechanism: str = "pool",
+    num_cores: Optional[int] = None,
+) -> List[Any]:
     """
     Executes function calls with list of parameters using specified mechanism.
 
     param func: The function
     param params: The parameters to use
     param mechanism: The mechanism
+    param num_cores: Number of cores to use
+
     return:
         Results of function executions
     """
     if mechanism == "pool":
-        return _pool(func=func, params=params)
+        return _pool(func=func, params=params, num_cores=num_cores)
     elif mechanism == "sequential":
         return _sequential(func=func, params=params, debug=False)
     elif mechanism == "debug":
