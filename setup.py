af970d68c6808d1a47adb310b205a4e874521745
Prateek Tandon <prateek1.tandon@gmail.com>
2023-04-16 21:37:30 -0700

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="algo-ops",
-    version="0.0.1.6.9",
+    version="0.0.1.7.0.1",
     author="Prateek Tandon",
     author_email="prateek1.tandon@gmail.com",
     description="Algo-Ops Algorithm Prototyping Framework",
